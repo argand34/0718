4dec917d508d44beb94ffde617358c88c56b2229
[SW5] 김경연 <argand0304@gmail.com>
2023-07-18 11:40:14 +0900

    리액트는 사용자 정의 태그인 컴포넌트를 만드는 기술이다.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Counter(){
+  return <div>
+    <h1>Counter</h1>
+    <button>+</button> 1
+  </div>
+}
+
 function App() {
   return (
     <div>
-      <h1>Counter</h1>
-      <button>+</button> 0
+      <Counter></Counter>
+      <Counter></Counter>
+      <Counter></Counter>
+      <Counter></Counter>
+      <Counter></Counter>
     </div>
   );
 }
